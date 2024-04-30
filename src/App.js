a4a86cb3fde34b5d2861b65102424ea20f70da65
Petar Nikolic <petar.nikolic@vyking.com>
2024-04-30 17:10:50 +0200

    Fix: removed unused code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import './App.scss';
 import WebSocketService from "./websocket";
 import Header from "./components/Header";
@@ -7,7 +7,6 @@ import Header from "./components/Header";
 function App() {
     const dispatch = useDispatch();
     const websocketInstance = WebSocketService.getInstance(dispatch);
-    const { isConnected, messages } = useSelector(state => state.websocket);
 
     useEffect(() => {
         websocketInstance.connect('wss://demo.vyking.com/ws');
@@ -20,9 +19,6 @@ function App() {
   return (
       <div className="App">
         <Header />
-        <main>
-          {/* Embed the WebSocket Component */}
-        </main>
       </div>
   );
 }
